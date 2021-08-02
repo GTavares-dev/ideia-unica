c0ee4e3e9ea17ed8b697c02f5f336067ae14ba91
Tavares <gabrieldvt@hotmail.com>
2021-08-02 17:49:31 -0300

    nova home3

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function Home() {
-  return <h1>Home 3. So, let's just write something down here</h1>
+  return (
+    <h1>
+      {' '}
+      Oi Lara meu amor, só queria dizer que você é o amor da mia vida todinha e
+      que me sinto muito muito feliz de estar ao seu lado!
+    </h1>
+  )
 }
 
 export default Home
